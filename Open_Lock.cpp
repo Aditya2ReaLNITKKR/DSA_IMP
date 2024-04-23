0874350898ff6478c0e6718249411c9575f1cad4
Aditya bhardwaj <bhardwajaditya394@gmail.com>
2024-04-23 20:27:19 +0530

    first commit

---
+++ Open_Lock.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+ void openLock(vector<string>& deadends, string target) {
+       cout<<"hello"<<endl;
+ }
+int main(){
+    vector<string>str{"0201","0101","0102","1212","2002"};
+    openLock(str,"hello");
+}
